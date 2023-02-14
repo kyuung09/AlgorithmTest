b4b9866a394fb45052fdc9c9f31c424908b8df98
Kyuu <117708164+kyuung09@users.noreply.github.com>
2023-02-14 15:04:30 +0900

    [unrated] Title: 옹알이 (2), Time: 0.05 ms, Memory: 9.97 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -0,0 +1,24 @@
+def solution(babbling):
+#     answer = 0
+#     words = ["aya", "ye", "woo", "ma"]
+
+    
+#     for i in babbling:
+#         for word in words:
+#             if word*2 not in i:
+#                 i = i.replace(word, "")
+#                 s = ""
+
+#         if len(i) == 0:
+#             answer += 1
+#     return answer
+
+    answer = 0
+    speak = ["aya","ye","woo","ma"]     #가능한 4가지 발음 리스트
+    for i in babbling:                  #babbling 문자열 배열만큼 반복
+        for v in speak:                 #4가지 발음으로 반복
+            if v*2 not in i:            #연속된 같은발음이 없다면
+                i=i.replace(v,' ')      #' '으로 발음을 치환
+        if i.strip()=='':               #치환 완료된 문자열의 공백을 제거하고 ''와 같다면
+            answer+=1                   #발음가능한 단어로 카운팅
+    return answer
