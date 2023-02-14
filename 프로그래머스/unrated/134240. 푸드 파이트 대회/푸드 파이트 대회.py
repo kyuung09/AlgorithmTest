5a7bbfb8a6616ac819b1165d453bca4f8233de1b
Kyuu <117708164+kyuung09@users.noreply.github.com>
2023-02-15 00:16:19 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,8 +1,3 @@
 def solution(food):
     answer = ''
-    
-    for i in range(1, len(food)):                  # food 배열의 수 만큼 반복
-        answer += str(i)*(food[i]//2)           # answer 배열에 i를 문자열 형태로 바꿔서 i를 2로 나눈 몫만큼 곱한만큼 저장한다
-        temp = ''.join(reversed(list(answer)))  # list에 있는 요소 하나하나를 합쳐서 반대로 저장한다.
-
-    return answer + '0' + temp
+    return answer
