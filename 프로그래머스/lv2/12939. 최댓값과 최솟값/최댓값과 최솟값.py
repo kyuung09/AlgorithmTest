ad821deaacdc360ff30b848a4cc0f417c1160910
Kyuu <117708164+kyuung09@users.noreply.github.com>
2023-03-07 21:06:57 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,4 +1,3 @@
 def solution(s):
-    arr = list(map(int, s.split(' ')))         # map을 사용하여 함수 두개 사용
-    arr.sort()                                  
+    arr = list(map(int, s.split(' ')))         # map을 사용하여 함수 두개 사용                           
     return str(min(arr)) + " " + str(max(arr))     
