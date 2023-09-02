789dcd9ad4c8e51ab7f38de870e280f63c7f7320
Kyuu <117708164+kyuung09@users.noreply.github.com>
2023-09-02 17:24:09 +0900

    [level 2] Title: 숫자의 표현, Time: 0.98 ms, Memory: 52.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+                for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            int num = i;
+
+            while (true) {
+                sum += num;
+
+                if (sum == n) {
+                    answer++;
+                    break;
+                } else if (sum > n) {
+                    break;
+                } else {
+                    num++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
