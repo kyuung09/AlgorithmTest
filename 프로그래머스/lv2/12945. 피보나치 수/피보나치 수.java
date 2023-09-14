fd0cc076a98f239134f76669afb146344fed343b
Kyuu <117708164+kyuung09@users.noreply.github.com>
2023-09-14 17:41:42 +0900

    [level 2] Title: 피보나치 수, Time: 2.37 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int solution(int n) {
+        return fibonacci(n) % 1234567;
+    }
+
+    private static int fibonacci(int n){
+        int[] answer = new int[n+1];
+        answer[1] = 1;
+
+        for (int i = 2 ; i <= n; i++ ){
+            answer[i] = (answer[i-2] + answer[i-1]) % 1234567;
+        }
+
+        return answer[n];
+    }
+}
